bc8af52df7ccb380f02f209751ed49257e1d55a9
samson onifade <107084481+samyCode3@users.noreply.github.com>
2022-09-06 07:01:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.set("view engine", "ejs")
-app.use("/home", (req, res) => {
+app.get("/home", (req, res) => {
    res.render('home', {
    key: PUBLISHABLE_KEY 
    })
